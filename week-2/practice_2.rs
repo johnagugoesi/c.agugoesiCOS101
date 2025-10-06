089e93dfe31a74c724ce54d34b3508bd3f71911f
johnagugoesi <chimdike.agugoesi@pau.edu.ng>
2025-10-07 00:11:35 +0100

    Add files via upload
    week2 practice assignments; 1,2,3 and 4

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+  println!(); //print just a
+
+  println!("hello ");
+
+  println!("format {} arguments", "some");
+
+  println!("My name os {} . I am the class rep of stream 1","Agugoesi John");
+}
