80e38b50871affa5a322b00b3dca542cf623e8ec
johnagugoesi <chimdike.agugoesi@pau.edu.ng>
2025-10-20 22:32:11 +0100

    practice 4 assignment 20-10-25

---
+++ practice_4/target/debug/deps/practice_4-eef801897410ea89.d
@@ -0,0 +1,5 @@
+/Users/ehitech/Documents/c.agugoesiCOS101/c.agugoesiCOS101/week-4/practice_4/target/debug/deps/practice_4-eef801897410ea89.d: src/main.rs
+
+/Users/ehitech/Documents/c.agugoesiCOS101/c.agugoesiCOS101/week-4/practice_4/target/debug/deps/practice_4-eef801897410ea89: src/main.rs
+
+src/main.rs:
