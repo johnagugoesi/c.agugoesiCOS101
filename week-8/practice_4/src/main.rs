a8e6a39325ae90cc72e9329ca6324d4ca56601e8
John Agugoesi <johnagugoesi@Johns-MacBook-Pro.local>
2025-11-17 21:11:22 +0100

    practice 4 assignment submit

---
+++ week-8/practice_4/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let name = vec!["Pary","Sary","Mary","Gary","Tary"];
+
+    let age = vec![15,13,16,18,27];
+    println!("\nAge Allocation\n");
+
+    for i in 0..age.len(){
+                        println!("{} is {} years old\n",name[i],age[i]);
+                        }
+}
