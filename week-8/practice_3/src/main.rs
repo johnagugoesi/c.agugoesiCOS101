f9fdb1578d246c91bdfc0c95cc3c3a757f4123e9
John Agugoesi <johnagugoesi@Johns-MacBook-Pro.local>
2025-11-17 21:10:50 +0100

    practice 3 assignment submit

---
+++ week-8/practice_3/src/main.rs
@@ -0,0 +1,18 @@
+fn value(ch:Option<&char>) {
+    println!("Element of vector {:?}",n);
+}
+
+
+fn main() {
+    let v = vec!['R','U','S','T','A','C','I','A','N'];
+
+    let mut in1 = String::new();
+    println!("\n Enter an index value between (0 - 8)");
+    std::io::stdin().read_line(&mut in1).expect("invalid input");
+
+    let index:usize = in1.trim().parse().expect("invalid input");
+
+    let ch:Option<&char> = v.get(index);
+    value(ch);
+
+}
